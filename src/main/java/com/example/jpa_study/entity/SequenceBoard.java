9c7f6ca829db233acfe856c7632efd4c0da0b34d
phantom08266 <phantom08266@gmail.com>
2022-07-14 00:06:37 +0900

    Comment [4장] Sequence 속성값 주석추가

--- src/main/java/com/example/jpa_study/entity/SequenceBoard.java
+++ src/main/java/com/example/jpa_study/entity/SequenceBoard.java
@@ -11,7 +11,8 @@ import javax.persistence.*;
 @SequenceGenerator(
         name = "BOARD_SEQ_GENERATOR",
         sequenceName = "BOARD_SEQ", // 매핑할 DB 시퀀스 이름
-        initialValue = 1, allocationSize = 1
+        initialValue = 1,           // 시작 시퀀스 값
+        allocationSize = 1          // 한번 호출에 증가하는 시퀀스 값
 )
 public class SequenceBoard {
 
