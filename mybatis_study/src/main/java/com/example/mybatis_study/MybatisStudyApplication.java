3f094b3c69e9b1e79026cb957f51b47500dd8094
phantom08266 <phantom08266@gmail.com>
2022-08-20 23:26:27 +0900

    Feat [mybatis] Alias 처리 및 불필요한 mapperscan제거

--- mybatis_study/src/main/java/com/example/mybatis_study/MybatisStudyApplication.java
+++ mybatis_study/src/main/java/com/example/mybatis_study/MybatisStudyApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.example.mybatis_study.**.repository")
 public class MybatisStudyApplication {
 
     public static void main(String[] args) {
--- mybatis_study/src/main/java/com/example/mybatis_study/center/domain/dao/Center.java
+++ mybatis_study/src/main/java/com/example/mybatis_study/center/domain/dao/Center.java
@@ -1,10 +1,12 @@
 package com.example.mybatis_study.center.domain.dao;
 
 import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
 
 import java.time.LocalDate;
 
 @NoArgsConstructor
+@Alias("Center")
 public class Center {
     private String name;
     private LocalDate regDt;
