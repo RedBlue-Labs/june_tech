cabdc033c535eb70b4cd1504fb3bc2ed550e40e2
phantom08266 <phantom08266@gmail.com>
2022-09-17 23:21:05 +0900

    JPA 11장 프로젝트 구성

---
+++ jpa_11_webApplication/src/test/java/com/example/jpa_11_webapplication/Jpa11WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_11_webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa11WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
